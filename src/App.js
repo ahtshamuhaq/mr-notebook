3816c4a661fd30df7c84ba2b231331e8838a333a
Ahtsham Ul Haq <112152384+ahtshamuhaq@users.noreply.github.com>
2022-12-12 18:11:01 +0500

    completed 12/12/22

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home } from "./components/Home";
+import About from "./components/About";
+import NoteState from "./Context/notes/NoteState";
+import { Alert } from "./components/Alert";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
-import './App.css';
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-
-} from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import NoteState from './Context/notes/NoteState';
 function App() {
   return (
     <>
-    <NoteState>
-    <Router>
-    <Navbar/>
-    <Routes>
-    <Route exact path='/'
-    element = {<Home/>}>
-      
-    </Route>
-    <Route exact path='/about'
-      element = {<About />}>
-
-    </Route>
-    </Routes>
-    </Router>
-    </NoteState>
-      </>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="This is amazing React course" />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/about" element={<About />}></Route>
+              <Route exact path="/login" element={<Login />}></Route>
+              <Route exact path="/signup" element={<Signup />}></Route>
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
+
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import Notes from './Notes';
 
-const Home = () => {
-  return (
-    <div>
-      This is home
-    </div>
-  )
+export const Home = () => {
+
+    return (
+        <div> 
+            <Notes/>
+        </div>
+    )
 }
-
-export default Home
